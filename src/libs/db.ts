5978e9085db1ded22bc3ca6c3cd61666d8936e56
mehm8128 <mehm8128@gmail.com>
2023-06-20 12:16:30 +0900

    環境変数修正

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -3,11 +3,11 @@ import { Connection, createConnection } from 'mysql2/promise'
 export async function connectDb(): Promise<Connection> {
 	try {
 		const connection = await createConnection({
-			host: process.env.NEXT_PUBLIC_NS_MARIADB_HOSTNAME,
-			port: Number(process.env.NEXT_PUBLIC_NS_MARIADB_PORT),
-			user: process.env.NEXT_PUBLIC_NS_MARIADB_USER,
-			password: process.env.NEXT_PUBLIC_NS_MARIADB_PASSWORD,
-			database: process.env.NEXT_PUBLIC_NS_MARIADB_DATABASE,
+			host: process.env.NS_MARIADB_HOSTNAME,
+			port: Number(process.env.NS_MARIADB_PORT),
+			user: process.env.NS_MARIADB_USER,
+			password: process.env.NS_MARIADB_PASSWORD,
+			database: process.env.NS_MARIADB_DATABASE,
 		})
 		return connection
 	} catch {
