d79ca69dc96054acd996c4a6c5c3f87aa3e31238
mehm8128 <mehm8128@gmail.com>
2023-06-23 23:18:40 +0900

    mockを使うように

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -9,35 +9,35 @@ import { TeaWithUnread } from '@/model/tea'
 import { styles } from './style.css'
 
 const useUnreads = async (): Promise<TeaWithUnread[]> => {
-	// return [
-	// 	{
-	// 		id: 'id2',
-	// 		count: 5,
-	// 		message: 'arigatou!',
-	// 		from: 'mehm8128',
-	// 		to: 'mehm8128',
-	// 		created_at: '2021-12-10T00:00:00.000Z',
-	// 		unread: true,
-	// 	},
-	// 	{
-	// 		id: 'id2',
-	// 		count: 5,
-	// 		message: 'arigatou!',
-	// 		from: 'mehm8128',
-	// 		to: 'mehm8128',
-	// 		created_at: '2021-12-10T00:00:00.000Z',
-	// 		unread: true,
-	// 	},
-	// 	{
-	// 		id: 'id2',
-	// 		count: 5,
-	// 		message: 'arigatou!',
-	// 		from: 'mehm8128',
-	// 		to: 'mehm8128',
-	// 		created_at: '2021-12-10T00:00:00.000Z',
-	// 		unread: true,
-	// 	},
-	// ]
+	return [
+		{
+			id: 'id2',
+			count: 5,
+			message: 'arigatou!',
+			from: 'mehm8128',
+			to: 'mehm8128',
+			created_at: '2021-12-10T00:00:00.000Z',
+			unread: true,
+		},
+		{
+			id: 'id2',
+			count: 5,
+			message: 'arigatou!',
+			from: 'mehm8128',
+			to: 'mehm8128',
+			created_at: '2021-12-10T00:00:00.000Z',
+			unread: true,
+		},
+		{
+			id: 'id2',
+			count: 5,
+			message: 'arigatou!',
+			from: 'mehm8128',
+			to: 'mehm8128',
+			created_at: '2021-12-10T00:00:00.000Z',
+			unread: true,
+		},
+	]
 	const res = await fetch(`${getApiOrigin()}/api/unreads`, {
 		next: { revalidate: 60 },
 	})
@@ -51,6 +51,8 @@ export default async function UserTea({
 	params: { userId: string }
 }) {
 	const unreads = await useUnreads()
+
+	if (!unreads) return <div>loading...</div>
 
 	return (
 		<div className={styles.container}>
