15c2cfa2c5688aec10887662dc7c4a8ea4238a9f
mehm8128 <mehm8128@gmail.com>
2023-06-24 21:35:41 +0900

    disable mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { TeaWithUnread } from '@/model/tea'
 import { User } from '@/model/user'
 
 const useUsers = async (): Promise<User[]> => {
-	return [{ name: 'mehm8128' }, { name: 'mehm8128128' }]
+	// return [{ name: 'mehm8128' }, { name: 'mehm8128128' }]
 	const res = await fetch(`${getApiOrigin()}/api/users`, {
 		next: { revalidate: 60 },
 	})
@@ -18,35 +18,35 @@ const useUsers = async (): Promise<User[]> => {
 }
 
 const useUnreads = async (): Promise<TeaWithUnread[]> => {
-	return [
-		{
-			id: 'id2',
-			count: 5,
-			message: 'arigatou!',
-			from: 'mehm8128',
-			to: 'mehm8128',
-			created_at: '2021-12-10T00:00:00.000Z',
-			unread: true,
-		},
-		{
-			id: 'id2',
-			count: 5,
-			message: 'arigatou!',
-			from: 'mehm8128',
-			to: 'mehm8128',
-			created_at: '2021-12-10T00:00:00.000Z',
-			unread: true,
-		},
-		{
-			id: 'id2',
-			count: 5,
-			message: 'arigatou!',
-			from: 'mehm8128',
-			to: 'mehm8128',
-			created_at: '2021-12-10T00:00:00.000Z',
-			unread: true,
-		},
-	]
+	// return [
+	// 	{
+	// 		id: 'id2',
+	// 		count: 5,
+	// 		message: 'arigatou!',
+	// 		from: 'mehm8128',
+	// 		to: 'mehm8128',
+	// 		created_at: '2021-12-10T00:00:00.000Z',
+	// 		unread: true,
+	// 	},
+	// 	{
+	// 		id: 'id2',
+	// 		count: 5,
+	// 		message: 'arigatou!',
+	// 		from: 'mehm8128',
+	// 		to: 'mehm8128',
+	// 		created_at: '2021-12-10T00:00:00.000Z',
+	// 		unread: true,
+	// 	},
+	// 	{
+	// 		id: 'id2',
+	// 		count: 5,
+	// 		message: 'arigatou!',
+	// 		from: 'mehm8128',
+	// 		to: 'mehm8128',
+	// 		created_at: '2021-12-10T00:00:00.000Z',
+	// 		unread: true,
+	// 	},
+	// ]
 	const res = await fetch(`${getApiOrigin()}/api/unreads`, {
 		next: { revalidate: 60 },
 	})
