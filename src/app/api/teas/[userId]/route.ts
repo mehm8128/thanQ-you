c49bcd35882a34f5f85c93ed0e5415947ec45a3e
mehm8128 <mehm8128@gmail.com>
2023-06-24 23:33:15 +0900

    traQへの通知を追加

--- src/app/api/teas/[userId]/route.ts
+++ src/app/api/teas/[userId]/route.ts
@@ -12,7 +12,10 @@ interface TeaTotalCount extends RowDataPacket {
 	total_count: number
 }
 
-export async function GET(req: NextRequest, context: { params: { userId: string } }) {
+export async function GET(
+	req: NextRequest,
+	context: { params: { userId: string } },
+) {
 	const userID = getShowcaseUser(req)
 	if (!userID) {
 		return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
@@ -23,9 +26,10 @@ export async function GET(req: NextRequest, context: { params: { userId: string 
 	try {
 		connection = await connectDb()
 
-		const [rows] = await connection.execute<TeaRow[]>('SELECT * FROM teas WHERE `to` = ?', [
-			targetUserId,
-		])
+		const [rows] = await connection.execute<TeaRow[]>(
+			'SELECT * FROM teas WHERE `to` = ?',
+			[targetUserId],
+		)
 		const [rows2] = await connection.execute<TeaTotalCount[]>(
 			'SELECT SUM(count) as total_count FROM teas WHERE `to` = ?',
 			[userID],
@@ -46,7 +50,10 @@ export async function GET(req: NextRequest, context: { params: { userId: string 
 	}
 }
 
-export async function POST(req: NextRequest, context: { params: { userId: string } }) {
+export async function POST(
+	req: NextRequest,
+	context: { params: { userId: string } },
+) {
 	const userID = getShowcaseUser(req)
 	if (!userID) {
 		return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
@@ -64,6 +71,39 @@ export async function POST(req: NextRequest, context: { params: { userId: string
 			'INSERT INTO teas (id, count, message, `from`, `to`, created_at, unread) VALUES (?, ?, ?, ?, ?, ?, ?)',
 			[uuid, data.count, data.message, userID, targetUserId, createdAt, true],
 		)
+
+		try {
+			const secret = process.env.NEXT_PUBLIC_TRAQ_ACCESS_TOKEN
+			const requestData = {
+				content: `@${userID} から${data.count}杯のお茶:tea:が注がれました\n>${data.message}`,
+				embed: true,
+			}
+			const traqUsersRes = await fetch(
+				`https://q.trap.jp/api/v3/users?name=${targetUserId}`,
+				{
+					headers: {
+						Authorization: `Bearer ${secret}`,
+					},
+				},
+			)
+			const targetTraqUser = (await traqUsersRes.json())[0]
+
+			await fetch(
+				`https://q.trap.jp/api/v3/users/${targetTraqUser.id}/messages`,
+				{
+					method: 'POST',
+					body: JSON.stringify(requestData),
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${secret}`,
+					},
+				},
+			)
+		} catch (e) {
+			if (e instanceof Error) {
+				throw new Error('Failed to send message to traQ: ' + e.message)
+			}
+		}
 
 		const res: Tea = {
 			id: uuid,
